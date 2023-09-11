b1f06fde2d0413437dbb6926633a80bd3ffe1185
idfp <bashed@tuta.io>
2023-09-11 08:42:07 +0700

    add prevention

--- src/components/GitProjects.vue
+++ src/components/GitProjects.vue
@@ -22,7 +22,7 @@ fetch("https://api.github.com/users/idfp/repos")
   .then((x) => {
     const holder:Array<GithubRepo> = []
     x.forEach( (y:GithubRepo) =>{
-      if(y.name === "corsa"){
+      if(y.name === "corsa" || y.name === "lnk-builder"){
         return
       }
       holder.push(y)
